cf12355b62ff94221a70bc3a8558e6e70846a23c
cerescredit <112599314+cerescredit@users.noreply.github.com>
2022-11-14 10:20:08 +0800

    Update CeresDID.sol

--- contracts/CeresDID.sol
+++ contracts/CeresDID.sol
@@ -17,9 +17,13 @@ contract CeresDID is CfoNftTakeable,ERC721Enumerable,NFTPropertyStorage,NFTMintM
     using SafeCast for uint256;
 
     mapping(uint256 => string) private _propertyNames;
+    
     mapping(bytes32 => string) private _propertyAlias;
+    
     mapping(uint256 => string) public tokenDIDOf;
+    
     mapping(bytes32 => uint256) public didTokenIdOf;
+    
     mapping(uint256 => string) public customImageOf;
     mapping(uint256 => address) public minterOf;
     mapping(address => uint256) public mintedTokenOf;
