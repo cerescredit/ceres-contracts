365d4376e21192cefde7b93b7dd6cc4ce53d6d7c
cerescredit <112599314+cerescredit@users.noreply.github.com>
2022-11-14 10:20:51 +0800

    Update CeresToken.sol

--- contracts/CeresToken.sol
+++ contracts/CeresToken.sol
@@ -15,15 +15,11 @@ contract CeresToken is CfoTakeable,Pausable,BlackListable,IERC20Metadata {
     
     
     mapping(address => uint256) private _balances;
-    
     mapping(address => mapping(address => uint256)) private _allowances;
-    
     //mainnet: 
     string private constant _name = "Ceres Token";
-    
     //mainnet: 
     string private constant _symbol = "Ceres";
-    
     uint256 private _totalSupply = 60000000 * 1e18;
     /// @notice The EIP-712 typehash for the contract's domain
     bytes32 public constant DOMAIN_TYPEHASH = keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
