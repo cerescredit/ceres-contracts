2a9a48e512bc0d4ab24a116a804deecef56ed1e1
cerescredit <112599314+cerescredit@users.noreply.github.com>
2022-11-21 10:05:43 +0800

    Update CeresDID.sol

--- contracts/CeresDID.sol
+++ contracts/CeresDID.sol
@@ -25,14 +25,23 @@ contract CeresDID is CfoNftTakeable,ERC721Enumerable,NFTPropertyStorage,NFTMintM
     mapping(bytes32 => uint256) public didTokenIdOf;
     
     mapping(uint256 => string) public customImageOf;
+    
     mapping(uint256 => address) public minterOf;
+    
     mapping(address => uint256) public mintedTokenOf;
+    
     mapping(uint256 => mapping(uint256 => string)) public stringPropertyOf;
+    
     bool public transferAllowed;
+    
     uint256 public constant allPackedPropsLength = 3; 
+    
     uint256 public constant allPropsLength = 6;
+    
     uint256 private constant allStringPropsLength = 6;
+    
     address public tokenDescriptor;
+    
     string public constant description = "Ceres DID is Decentralized digital identity system with programmable metadata and verifiable signatures.";
  
     event Minted(address minter,address to,uint tokenId,bytes32[] packedProps,string did);
