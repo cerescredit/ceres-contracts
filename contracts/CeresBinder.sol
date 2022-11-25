d469e2d875a9c19a4a4259ce1baff277605c9755
cerescredit <112599314+cerescredit@users.noreply.github.com>
2022-11-25 16:30:10 +0800

    Update CeresBinder.sol

--- contracts/CeresBinder.sol
+++ contracts/CeresBinder.sol
@@ -19,11 +19,8 @@ contract CeresBinder is CfoTakeable,Adminable,ReentrancyGuard {
     using SafeMath for uint256;
 
     address public immutable ceresRelation;
-    
     IDID public immutable ceresDID;
-    
     address public rewardsToken;
-    
     uint public selfAmount;
     uint public parentAmount;
     uint public grandpAmount;
