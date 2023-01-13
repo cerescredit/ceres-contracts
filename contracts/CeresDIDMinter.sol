be0a32c255c82d9ffe8b683c4e67c311a1738a81
cerescredit <112599314+cerescredit@users.noreply.github.com>
2023-01-13 21:09:03 +0800

    Update CeresDIDMinter.sol

--- contracts/CeresDIDMinter.sol
+++ contracts/CeresDIDMinter.sol
@@ -21,13 +21,16 @@ interface IDIDNFT {
 contract DIDMinter is CfoNftTakeable,Adminable,Pausable,ReentrancyGuard {
 
     IDIDNFT public immutable ceresDIDNFT;
+    
     uint public mintBNBFee;
+    
     event Minted(address caller,uint tokenId);
     
     constructor(
         address _ceresDIDNFT
     ) {
         require(_ceresDIDNFT != address(0),"_ceresDIDNFT vault can not be 0");
+        
         ceresDIDNFT = IDIDNFT(_ceresDIDNFT);
     }
 
