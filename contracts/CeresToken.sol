d8ab3982dff8b222d0c2706462b58a7836c6fc06
Ceres <112599314+cerescredit@users.noreply.github.com>
2023-01-25 14:35:48 +0800

    Update CeresToken.sol

--- contracts/CeresToken.sol
+++ contracts/CeresToken.sol
@@ -10,13 +10,10 @@ import './libs/CfoTakeable.sol';
 import './libs/BlackListable.sol';
 
 contract CeresToken is CfoTakeable,Pausable,BlackListable,IERC20Metadata {
-
     using Address for address;
-
     using SafeMath for uint256;
     
     mapping(address => uint256) private _balances;
-    
     mapping(address => mapping(address => uint256)) private _allowances;
     //mainnet: 
     string private constant _name = "Ceres Token";
